e19eeacb711ecc1847f6693269b5d87c100a5720
Andre da Silva <adasi5@uis.edu>
2025-04-27 15:08:14 -0400

    naming consistency

--- frontend/src/pages/dashboard/JobListings.js
+++ frontend/src/pages/dashboard/JobListings.js
@@ -425,7 +425,7 @@ const JobListings = () => {
 
                         <div>
                             <label className="block text-sm font-medium text-gray-700 mb-1">
-                                Job Type
+                                Job Level
                             </label>
                             <select
                                 name="level"
